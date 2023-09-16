1fe53798f14546cf29c1cbb4e21e92aaa205fa42
koki221350 <141555964+koki221350@users.noreply.github.com>
2023-09-16 18:38:52 +0900

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles.css";
+
+export const Header = () => {
+  return (
+    <>
+      <header>
+        <div class="header">
+          <p>YOBIWAVE</p>
+        </div>
+      </header>
+
+      <div class="container">
+        <h1>YOBIWAVE</h1>
+        <h3>Check the Environmental Impact</h3>
+      </div>
+
+      <div class="input-area">
+        <input placeholder="商品を入力" />
+        <button>検索</button>
+      </div>
+    </>
+  );
+};
+
+export default Header;
